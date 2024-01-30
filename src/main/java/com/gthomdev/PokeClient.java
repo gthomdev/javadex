2209e1fa1704863dbb007607236eda1a71139b04
George Thomas <georgesilverbeech@gmail.com>
2024-01-30 22:38:41 +0000

    Added POC for image retrieval (non-generic case)

--- src/main/java/com/gthomdev/PokeClient.java
+++ src/main/java/com/gthomdev/PokeClient.java
@@ -1,11 +1,14 @@
 package com.gthomdev;
 
 import com.gthomdev.model.Pokemon;
+import com.gthomdev.service.PokemonImageService;
 import com.gthomdev.service.PokemonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
+
+import java.awt.image.BufferedImage;
 
 @SpringBootApplication
 public class PokeClient {
@@ -14,14 +17,22 @@ public class PokeClient {
     }
 
     @Bean
-    public CommandLineRunner run(PokemonService pokemonService) throws Exception {
+    public CommandLineRunner run(PokemonService pokemonService, PokemonImageService pokemonImageService) throws Exception {
         return args -> {
+
+            BufferedImage bulbasaur = pokemonImageService.getPokemonImageForPokemon("bulbasaur");
+            if (bulbasaur != null) {
+                System.out.println(bulbasaur);
+            }
+            /*
+
             for (int i = 1; i < 152; i++) {
                 Pokemon pokemon = pokemonService.getPokemonById(Integer.toString(i));
                 StringBuilder sb = new StringBuilder();
                 sb.append("Pokemon ").append(pokemon.name).append(" has ID ").append(pokemon.id);
                 System.out.println(sb);
             }
-        };
-    }
+        }; */
+    };
+}
 }
